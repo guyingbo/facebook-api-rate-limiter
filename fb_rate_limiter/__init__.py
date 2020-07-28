32b9e683c07c130c7b3a5eb3d15899407f2c6d7b
Gu Yingbo <tensiongyb@gmail.com>
2020-07-28 15:25:20 +0800

    feat: smoother concurrency control

--- fb_rate_limiter/__init__.py
+++ fb_rate_limiter/__init__.py
@@ -1,9 +1,10 @@
 import json
+import time
 from enum import Enum
 from threading import Condition
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
-from cachetools import TTLCache
+from cachetools import Cache
 
 __version__ = "0.0.1"
 
@@ -18,18 +19,72 @@ class BUCRateLimitType(Enum):
     pages = "pages"
 
 
-class MyTTLCache(TTLCache):
-    def ttl_set(self, key, value, ttl):
-        origin_ttl = self._TTLCache__ttl
-        self._TTLCache__ttl = ttl
-        self[key] = value
-        self._TTLCache__ttl = origin_ttl
+class SandglassCache(Cache):
+    """
+    SandglassCache is a special kind of cache that reduces its value as time passes.
+    @param sink_rate: how much amount reduces each time
+    @param sink_seconds: how often each reduce occurs
+    @param lower_limit: lower limit of the value
+    """
+
+    def __init__(
+        self,
+        maxsize: int,
+        *,
+        sink_rate: int,
+        sink_seconds: int,
+        lower_limit: Union[int, float] = float("-inf")
+    ):
+        super().__init__(maxsize)
+        self.lower_limit = lower_limit
+        self.sink_rate = sink_rate
+        self.sink_seconds = sink_seconds
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, (value, time.monotonic()))
+
+    def __getitem__(self, key):
+        value, timestamp = super().__getitem__(key)
+        elapsed = time.monotonic() - timestamp
+        if elapsed > 0:
+            n = int(elapsed // self.sink_seconds)
+            value -= self.sink_rate * n
+            return max(value, self.lower_limit)
+        return value
+
+    def set_and_freeze(self, key, value, freeze_seconds: Union[int, float]):
+        """freeze a value for some time"""
+        super().__setitem__(key, (value, time.monotonic() + freeze_seconds))
+
+    def incr(self, key, value):
+        val, ts = super().__getitem__(key)
+        super().__setitem__(key, (val + value, ts))
 
 
 class Strategy:
-    def __init__(self, maxsize: int = 10000, ttl: int = 60, threshold: int = 80):
-        self._cache = MyTTLCache(maxsize=maxsize, ttl=ttl)
+    def __init__(self, cache: SandglassCache, threshold: int = 80, increase_rate=5):
+        self._cache = cache
         self._threshold = threshold
+        self._increase_rate = increase_rate
+
+    @classmethod
+    def new(
+        cls,
+        maxsize: int = 10000,
+        sink_rate: int = 1,
+        sink_seconds: int = 10,
+        lower_limit: int = 0,
+        threshold: int = 80,
+    ):
+        return cls(
+            SandglassCache(
+                maxsize,
+                sink_rate=sink_rate,
+                sink_seconds=sink_seconds,
+                lower_limit=lower_limit,
+            ),
+            threshold=threshold,
+        )
 
     def update_from_headers(self, headers: dict):
         # {"business_id_xxx":[{"type":"ads_insights","call_count":1,"total_cputime":1,"total_time":1,"estimated_time_to_regain_access":0}]}'
@@ -43,12 +98,12 @@ class Strategy:
                         "estimated_time_to_regain_access"
                     )
                     if estimated_time_to_regain_access == 0:
-                        ttl = self._cache.ttl
+                        freeze_seconds = 0
                     else:
-                        ttl = (estimated_time_to_regain_access + 1) * 60
+                        freeze_seconds = estimated_time_to_regain_access * 60
                     max_percentage = max(usage_type_dic.values())
-                    self._cache.ttl_set(
-                        (business_object_id, usage_type), max_percentage, ttl
+                    self._cache.set_and_freeze(
+                        (business_object_id, usage_type), max_percentage, freeze_seconds
                     )
         # ad account usage strategy
         if "x-ad-account-usage" in headers:
@@ -67,7 +122,11 @@ class Strategy:
             self._cache["fb-ads-insights-throttle"] = max_percentage
 
     def check_keys(self, keys: List[Tuple[str, str]]) -> bool:
-        return all(self.check(key) for key in keys)
+        r = all(self.check(key) for key in keys)
+        if r:
+            for key in keys:
+                self._cache.incr(key, self._increase_rate)
+        return r
 
     def check(self, key: Tuple[str, str]) -> bool:
         percentage = self._cache.get(key)
